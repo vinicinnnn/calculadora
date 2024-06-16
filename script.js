0c868327d3ffc7a5ff67b57ed48994bff825dc84
vinicinnnn <1vinizada@gmail.com>
2024-06-16 14:31:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+function calculate() {
+    var display = document.getElementById("display").value;
+    var result = eval(display);
+    document.getElementById("display").value = result;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
